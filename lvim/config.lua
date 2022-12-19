7297948a3678b163c562fc7f5c9155c703cf52a8
Jonathan Flatt <jonflatt@gmail.com>
2022-12-18 20:44:22 -0600

    update lvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,5 +1,3 @@
-
--- vim.lsp.set_log_level("debug")
 --[[
 lvim is the global options object
 
@@ -11,169 +9,72 @@ an executable
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
-
-vim.opt.dictionary:append("/usr/share/dict/words")
 lvim.log.level = "warn"
-lvim.format_on_save = true
 lvim.colorscheme = "tokyonight"
--- nvim.clipboard = 'win32yank'
+vim.opt.dictionary:append("/usr/share/dict/words")
+vim.opt.clipboard = "unnamedplus"
 
+vim.format_on_save = {
+  ---@usage pattern string pattern used for the autocommand (Default: '*')
+  pattern = "*",
+  ---@usage timeout number timeout in ms for the format request (Default: 1000)
+  timeout = 5000,
+  ---@usage filter func to select client
+}
+
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
-  lvim.keys = {
-    ---@usage change or add keymappings for insert mode
-    insert_mode = {
-      ["<A-e>"] = "<Esc>:m .+1<CR>==gi",
-      ["<A-i>"] = "<Esc>:m .-2<CR>==gi",
-      -- navigation
-      -- ["<A-Up>"] = "<C-\\><C-N><C-w>k",
-      -- ["<A-Down>"] = "<C-\\><C-N><C-w>j",
-      -- ["<A-Left>"] = "<C-\\><C-N><C-w>h",
-      -- ["<A-Right>"] = "<C-\\><C-N><C-w>l",
-    },
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.insert_mode["kj"] = false -- Better Escape doesnt handle kj delay inputvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.builtin.terminal.open_mapping = [[<c-t>]]
 
-    ---@usage change or add keymappings for normal mode
-    normal_mode = {
-      ["<C-s>"] = ":w<cr>",
-      ["<F15>"] = '<cmd>luarequire("harpoon.ui").toggle_quick_menu()<cr>',
-      -- Better window movement
-      ["{"] = "<C-w>h",
-      ["_"] = "<C-w>j",
-      ["!"] = "<C-w>k",
-      ["*"] = "<C-w>l",
-
-      -- Tab switch buffer
-      ["<A-n>"] = ":BufferNext<CR>",
-      ["<A-o>"] = ":BufferPrevious<CR>",
-      -- Move current line / block with Alt-j/k a la vscode.
-      ["<A-e>"] = ":m .+1<CR>==",
-    ["<A-i>"] = ":m .-2<CR>==",
-
-      -- insert empty line below/above without going into insert mode
-      ["m"] = "yyp0D",
-      ["M"] = "yykp0D",
-
-      -- Resize with arrows
-      ["<A-I>"] = ":resize -2<CR>",
-      ["<A-E>"] = ":resize +2<CR>",
-      ["<A-N>"] = ":vertical resize -2<CR>",
-    ["<A-O>"] = ":vertical resize +2<CR>",
-
-
-      -- QuickFix
-      ["]q"] = ":cnext<CR>",
-      ["[q"] = ":cprev<CR>",
-      ["<C-q>"] = ":call QuickFixToggle()<CR>",
-    },
-
-    ---@usage change or add keymappings for terminal mode
-    term_mode = {
-      -- Terminal window navigation
-      ["<C-h>"] = "<C-\\><C-N><C-w>h",
-      ["<C-j>"] = "<C-\\><C-N><C-w>j",
-      ["<C-k>"] = "<C-\\><C-N><C-w>k",
-      ["<C-l>"] = "<C-\\><C-N><C-w>l",
-    },
-
-    ---@usage change or add keymappings for visual mode
-    visual_mode = {
-      -- Better indenting
-      ["<"] = "<gv",
-      [">"] = ">gv",
-
-      -- ["p"] = '"0p',
-      -- ["P"] = '"0P',
-    },
-
-    ---@usage change or add keymappings for visual block mode
-    visual_block_mode = {
-      -- Move selected line / block of text in visual mode
-      ["I"] = ":move '<-2<CR>gv-gv",
-      ["E"] = ":move '>+1<CR>gv-gv",
-
-      -- Move current line / block with Alt-j/k ala vscode.
-      ["<A-e>"] = ":m '>+1<CR>gv-gv",
-      ["<A-i>"] = ":m '<-2<CR>gv-gv",
-    },
-
-    ---@usage change or add keymappings for command mode
-    command_mode = {
-      -- navigate tab completion with <c-j> and <c-k>
-      -- runs conditionally
-      ["<C-j>"] = { 'pumvisible() ? "\\<C-n>" : "\\<C-j>"', { expr = true, noremap = true } },
-      ["<C-k>"] = { 'pumvisible() ? "\\<C-p>" : "\\<C-k>"', { expr = true, noremap = true } },
-    },
-  }
-
--- lvim.keys.normal_mode["<leader>,"] = ':lua require"nvim-sfdx"<cr>'
 -- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+-- vim.keymap.del("n", "<C-Up>")
+-- override a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
 --   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
 --   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
   name = "+Trouble",
   r = { "<cmd>Trouble lsp_references<cr>", "References" },
   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
-}
--- lvim.builtin.which_key.mappings["j"] = {
---   name = "Harpoon",
---   a = {'<cmd>lua require("harpoon.mark").add_file()<cr>', 'add file'},
---   m = {'<cmd>luarequire("harpoon.ui").toggle_quick_menu()<cr>', 'toggle menu'},
---   h = {'<cmd>luarequire("harpoon.ui").nav_file(1)<cr>', 'Index 1'},
---   j = {'<cmd>luarequire("harpoon.ui").nav_file(2)<cr>', 'Index 2'},
---   k = {'<cmd>luarequire("harpoon.ui").nav_file(3)<cr>', 'Index 3'},
---   l = {'<cmd>luarequire("harpoon.ui").nav_file(4)<cr>', 'Index 4'}
--- }
-lvim.keys.normal_mode["F15"] = '<cmd>luarequire("harpoon.ui").nav_file(1)<cr>'
-lvim.keys.normal_mode["F16"] = '<cmd>luarequire("harpoon.ui").nav_file(2)<cr>'
-lvim.keys.normal_mode["F17"] = '<cmd>luarequire("harpoon.ui").nav_file(3)<cr>'
-lvim.keys.normal_mode["F18"] = '<cmd>luarequire("harpoon.ui").nav_file(4)<cr>'
-
-lvim.builtin.which_key.mappings["j"] = {
-  name = "jettings",
-  t = {'<cmd>TransparentToggle<cr>', 'TransparentToggle'},
-}
-
-lvim.builtin.which_key.mappings["m"] = {
-  name = "Macros",
-  d = {'A<BS>r:\\DAS_SFDC_ID__c<esc>', 'DAS_SFDC_ID__c'},
-  m = {'I<members><esc>A</members><esc>:', '<members>'},
-}
-
-lvim.builtin.which_key.mappings["y"] = {
-  name = "Symbols",
-  c = {'i<esc>', ''},
-  e = {'i<esc>', ''},
-  d = {'i<esc>', ''}
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+-- lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -184,90 +85,156 @@ lvim.builtin.treesitter.ensure_installed = {
   "lua",
   "python",
   "typescript",
+  "tsx",
   "css",
   "rust",
   "java",
-  "yaml"
+  "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
-lvim.builtin.cmp.sources = {
-  {name = "nvim_lsp"},
-  {name = "nvim_lua"},
-  {name = "path"},
-  {name = "luasnip"},
-  {name = "buffer", keyword_length = 4 },
-  {name = "dictionary", keyword_length = 4 }
-}
+-- generic LSP settings
 
-lvim.builtin.cmp.experimental = {
-  native_menu = false,
-  ghost_menu = true
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+lvim.lsp.installer.setup.ensure_installed = {
+  "sumeko_lua",
+  "jsonls",
+  "null-ls"
 }
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     uninstall_server = "d",
+--     toggle_server_expand = "o",
+-- }
 
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.installer.setup.automatic_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup({{exe = "prettier", filetypes = {"apex"} }})
-lvim.format_on_save = true
+formatters.setup {
+  { command = "prettier", extra_args = { '--plugin', 'prettier-plugin-apex' }, filetypes = { "apexcode" } },
+}
+-- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+---@usage arguments to pass to the formatter
+-- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--   extra_args = { "--print-with", "100" },
+--   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--   filetypes = { "typescript", "typescriptreact" },
+-- },
+-- }
 
-local lspconfig = require'lspconfig'
-local configs = require'lspconfig/configs'
-local apexCmd = { '/usr/bin/java','-cp', '/home/jonflatt/languageServers/Apex/apex-jorje-lsp.jar', '-Ddebug.internal.errors=true', 'apex.jorje.lsp.ApexLanguageServerLauncher' }
--- Check if it's already defined for when reloading this file.
-if not lspconfig.apexls then
-  configs.apexls = {
-    default_config = {
-      cmd = apexCmd,
-      filetypes = {'apex'},
-      root_dir = require("lspconfig/util").root_pattern(".git", "package.json"),
-      on_attach = require("lvim.lsp").common_on_attach,
-      on_init = require("lvim.lsp").common_on_init,
-      capabilities = require("lvim.lsp").common_capabilities(),
-      formatters = { exe = 'prettier' },
-      settings = {}
-    }
-  }
-end
-lspconfig.apexls.setup{}
-
---Powershel LSP
-if not lspconfig.powershell_es then
-  configs.powershell_es = {
-      setup = {
-  bundle_path = '/home/jonflatt/languageServers/Powershell',
-    }
-  }
-end
-
---Powershell treesitter
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.java.used_by = "apex"
+-- -- set additional linters
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "shellcheck",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--severity", "warning" },
+--   },
+--   {
+--     command = "codespell",
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "javascript", "python" },
+--   },
+-- }
 
 -- Additional Plugins
 lvim.plugins = {
-  {'chrisbra/csv.vim'},
-  {'airblade/vim-gitgutter'},
-  {'nvim-treesitter/playground'},
-  {'folke/tokyonight.nvim'},
-  {'folke/trouble.nvim'},
-  {'ThePrimeagen/harpoon'},
-  {'uga-rosa/cmp-dictionary'},
-  {'Cheffromspace/nvim-sfdx'},
-  {'norcalli/nvim-colorizer.lua'},
-  {'xiyaowong/nvim-transparent'}
-  -- {'github/copilot.vim'}
+  {
+    { "zbirenbaum/copilot.lua",
+      event = { "VimEnter" },
+      config = function()
+        vim.defer_fn(function()
+          require("copilot").setup {
+            plugin_manager_path = get_runtime_dir() .. "/site/pack/packer"
+          }
+        end, 100)
+      end,
+    },
+    { "zbirenbaum/copilot-cmp",
+      after = { "copilot.lua", "nvim-cmp" },
+      config = function()
+        require("copilot_cmp").setup({ filetypes = { [{ "*" }] = true } })
+      end
+    },
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  { "folke/todo-comments.nvim" },
+  { "Cheffromspace/sfdx-deploy" }
 }
 
-require'colorizer'.setup()
+-- Can not be placed into the config method of the plugins.
+lvim.builtin.cmp.formatting.source_names["copilot"] = "(Copilot)"
+table.insert(lvim.builtin.cmp.sources, 1, { name = "copilot" })
 
--- lvim.keys.insert_mode['<right>'] = '<cmd>lua nvim_exec(copilot#Accept())<cr>'
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.apex", "*.cls", "*.trigger" },
+  command = "setlocal filetype=apexcode",
+})
 
--- vim.api.nvim_exec([[
---   imap <script><silent><nowait><expr> <right> <cmd> copilot#Accept()
--- ]])
--- <script><silent><nowait><expr> <Tab> copilot#Accept()
---Autocommands (https://neovim.io/doc/user/autocmd.html)
-lvim.autocommands.custom_groups = {
-  { "BufWinEnter", "*.cls", "setlocal tabstop=4 softtabstop=4 shiftwidth=4 cindent"}
-}
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.page" },
+  command = "setlocal filetype=html",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "zsh",
+  callback = function()
+    -- let treesitter use bash highlight for zsh files as well
+    require("nvim-treesitter.highlight").attach(0, "bash")
+  end,
+})
+
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 0
+
+local parser_config = require "nvim-treesitter.parsers".filetype_to_parsername
+parser_config.apexcode = "java"
+
+-- Convert vimscript to lua
+-- nnoremap <M-,> :call search('^'. matchstr(getline('.'), '\(^\s*\)') .'\%<' . line('.') . 'l\S', 'be')<CR>
+-- nnoremap <M-.> :call search('^'. matchstr(getline('.'), '\(^\s*\)') .'\%>' . line('.') . 'l\S', 'e')<CR>
+
+vim.api.nvim_set_keymap("n", "<M-,>",
+  ":call search('^'. matchstr(getline('.'), '\\(\\^\\s*\\)') .'\\%<' . line('.') . 'l\\S', 'be')<CR>",
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<M-.>",
+  ":call search('^'. matchstr(getline('.'), '\\(\\^\\s*\\)') .'\\%>' . line('.') . 'l\\S', 'e')<CR>",
+  { noremap = true, silent = true })
+
+-- map leader D to lua require("sfdx_deploy").deploy_current_file(
+vim.api.nvim_set_keymap("n", "<leader>D", ":lua require('sfdx_deploy').deploy_current_file()<CR>",
+  { noremap = true, silent = true })
